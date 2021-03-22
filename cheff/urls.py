970e0ca9fed56d22af4784e3752f62b97c3edf24
mendesbayout <62385787+mendesbayout@users.noreply.github.com>
2021-03-22 16:35:43 -0300

    master3

--- cheff/urls.py
+++ cheff/urls.py
@@ -11,16 +11,17 @@ schema_view = get_schema_view(
       title="Cheff API backend exam WORKALOVE",
       
       default_version='beta backend',
-      description="Esta interface é a conexão entre você e dois dos melhores cheff's do mundo, um é especialista em FastFood, seu nome é Vinsmoke, o outro, é especialista em receitas demoradas, ele se chama Zeff, você comprou esse contato direto com eles por 5 milhões de dolares. Essa API fornece também a possibilidade de integração com Qualquer sistema de frontend.Caso tenha algum problema, acesse o dominio http://127.0.0.1:8000/admin/, as credenciais devem ser criadas.",
+      description="Esta interface é a conexão entre você e dois dos melhores cheff's do mundo, um é especialista em FastFood, seu nome é Vinsmoke, o outro, é especialista em receitas demoradas, ele se chama Zeff, você comprou esse contato direto com eles por 5 milhões de dolares. Essa API fornece também a possibilidade de integração com Qualquer sistema de frontend.Caso tenha algum problema, acesse o dominio http://127.0.0.1:8000/admin/, as credenciais devem ser criadass",
       terms_of_service="https://github.com/mendesbayout",
       contact=openapi.Contact(email="mendesbayout@gmail.com"),
-      license=openapi.License(name="Como o test é de backend voltado para Django Rest, acho que um Swagger funcional pode ser chamado de CRUD Interface. 1 - GET>TRY>Execute to meet their Ids"),
+      license=openapi.License(name="Como o test é de backend voltado para Django Rest, acho que um Swagger funcional pode ser chamado de CRUD Interface"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('api', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
